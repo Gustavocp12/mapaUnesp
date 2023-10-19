2a678e387a6c05d62967072c0077ffd189387c19
Gustavo Costa <gustavo.costa@lojainterativa.com>
2023-10-18 23:54:59 -0300

    mapa estilizado com funcionalidades comentadas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,42 +1,88 @@
-import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit, AfterViewInit{
+
+export class AppComponent implements AfterViewInit{
   title = 'mapaUnesp';
 
+  //referenciar o mapa no html
   @ViewChild('map', {static: false}) gmap!: ElementRef;
 
-  ngOnInit(): void {
-  }
-
+  //ciclo de vida que inicializa apos a view ser carregada
   ngAfterViewInit(): void {
     this.mapInitializer();
   }
 
+  //função do mapa
   mapInitializer(): void {
+
+    //variaveis de controle do mapa
+
+      //limites do mapa
     const allowedBounds = new google.maps.LatLngBounds(
-      new google.maps.LatLng(-22.0150, -48.9400), // Coordenadas do canto inferior esquerdo
-      new google.maps.LatLng(-22.0080, -48.9300)  // Coordenadas do canto superior direito
+      new google.maps.LatLng(-21.266389, -50.547887),
+      new google.maps.LatLng(-21.105210, -50.370498)
     );
-    const coordinates = new google.maps.LatLng(-21.23352988542779, -50.458202039401094);
+
+      //ajustar a centralização do mapa
+    const centerLat = (-21.266389 + -21.105210) / 2;
+    const centerLng = (-50.547887 + -50.370498) / 2;
+    const adjustmentLat = 0.02;
+    const adjustmentLng = 0.01;
+    const adjustedCenterLat = centerLat - adjustmentLat;
+    const adjustedCenterLng = centerLng + adjustmentLng;
+    const coordinates = new google.maps.LatLng(adjustedCenterLat, adjustedCenterLng);
+
+      //estilo do mapa
+    const mapStyles = [
+      {
+        featureType: 'poi',
+        stylers: [{ visibility: 'off' }]
+      }
+    ];
+
+     //opções do mapa
     const mapOptions: google.maps.MapOptions = {
       center: coordinates,
       zoom: 13,
+      styles: mapStyles,
+      restriction: {
+        latLngBounds: allowedBounds,
+        strictBounds: true
+     }
     };
+
+     //gerar o mapa
     const map = new google.maps.Map(this.gmap.nativeElement, mapOptions);
+
+     //informações dos pontos
+    const infoWindow = new google.maps.InfoWindow();
+
+      //carregar o geojson
     map.data.loadGeoJson('assets/data.geojson');
-    map.data.setStyle({
-      fillColor: 'blue',
-      strokeWeight: 0
+
+      //estilo do mapa - geojson
+    map.data.setStyle((feature: any) =>{
+      const isMarker = feature.getGeometry().getType() === 'Point';
+      return {
+        fillColor: 'blue',
+        strokeWeight: 1
+      };
     });
-    // map.setOptions({
-    //   restriction: {
-    //     latLngBounds: allowedBounds,
-    //   }
-    // })
+
+      //evento de click - infoWindow
+    map.data.addListener('click', (event: any) => {
+      const geometryType = event.feature.getGeometry().getType();
+      if (geometryType === 'Point') {
+        const content = event.feature.getProperty('name') || 'Nome não disponível';
+        infoWindow.setContent(content);
+        infoWindow.setPosition(event.feature.getGeometry().get());
+        infoWindow.open(map);
+      }
+    });
   }
 }
